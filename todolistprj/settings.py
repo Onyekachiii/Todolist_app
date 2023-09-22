414e2dd4067700de611340403392a351524504c2
Onyekachiii <stiloonwa4u@yahoo.com>
2023-09-22 10:49:54 +0400

    Added Render to allowed hosts

--- todolistprj/settings.py
+++ todolistprj/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-09r=h)h8aqz&me%4(m1jv!qq2vku6p&$*&=@yu+-#92vg2e=gj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['onyeka-todo-list.onrender.com']
 
 
 # Application definition
